55522830ab839ab5f3d9ead49c245cd1d3712cc9
brooks !! <144816371+Brooks1228@users.noreply.github.com>
2024-02-01 08:12:07 -0500

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,21 @@
+﻿Imports System.Media
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim rand As New Random
+
+        For i As Integer = 0 To 50
+            Dim bad = New Form
+            bad.Name = "hiiiiii"
+            Dim x = rand.Next(My.Computer.Screen.Bounds.Width)
+            Dim y = rand.Next(My.Computer.Screen.Bounds.Height)
+            bad.BackgroundImage = My.Resources.gnarpy_improved
+            bad.BackgroundImageLayout = ImageLayout.Zoom
+            bad.Location = New Point(x, y)
+            bad.StartPosition = FormStartPosition.Manual
+            bad.Show()
+        Next
+
+        My.Computer.Audio.Play("C:\Windows\Media\Alarm01.wav")
+    End Sub
+End Class
